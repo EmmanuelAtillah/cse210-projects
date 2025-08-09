f8ff5b880c9894ea5bd1acc5ba4db137b0388f8c
Emmanuel Atillah <emmauelatillahluv@gmail.com>
2025-08-09 05:45:55 +0000

    uploading shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.Clear();
+        Square square = new Square("blue", 6);
+        Console.WriteLine($"Color: {square.GetColor()}");
+        Console.WriteLine($"The area of the square is: {square.GetArea()}\n");
+
+        Rectangle rectangle = new Rectangle("red", 4, 3);
+        Console.WriteLine($"Color: {rectangle.GetColor()}");
+        Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea()}\n");
+
+        Circle circle = new Circle("crympson", 5);
+        Console.WriteLine($"Color: {circle.GetColor()}");
+        Console.WriteLine($"The area of the circle is: {circle.GetArea()}");
     }
 }
